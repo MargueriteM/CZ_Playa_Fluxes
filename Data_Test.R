e169e0979cfaab21d85e89ec30c1fd31b810bbf2
Marguerite Mauritz <mauritzmarguerite@gmail.com>
2021-09-29 22:48:24 -0600

    add flux data from output and summaries folder

--- Data_Test.R
+++ Data_Test.R
@@ -6,17 +6,53 @@ library(dplyr)
 library(ggplot2)
 library(lubridate)
 library(stringr)
+library(data.table)
 
 # get windrose function
 source("~/Desktop/R/R_programs/Functions/plot.windrose.R")
 
 # set working directory to One Drive folder with data (folder belongs to Marguerite Mauritz)
-setwd("~/Desktop/OneDrive - University of Texas at El Paso/CZ_Drylands/JER_RedLakePlaya/Data")
+setwd("~/Desktop/OneDrive - University of Texas at El Paso/CZ_Drylands/JER_RedLakePlaya/Data/RoofTest")
 
 # read column names and import data
 biomet.head <- colnames(read.table("CR3000 Red Lake Remote Connect_Biomet.dat", sep=",", dec=".", skip=1, header=TRUE))
 biomet <- read.table("CR3000 Red Lake Remote Connect_Biomet.dat", sep=",", dec=".", skip=4, header=FALSE,
                      col.names=biomet.head, na.strings=c("NAN"))
+
+# load flux results data from Smartflux system
+library(plyr)
+setwd("~/Desktop/OneDrive - University of Texas at El Paso/CZ_Drylands/JER_RedLakePlaya/Data/EddyCovariance_SmartFlux/results/2021/09")
+
+# create an output directory for unzipped files using plyr
+# https://stackoverflow.com/questions/41954183/how-can-i-extract-multiple-zip-files-and-read-those-csvs-in-r/41954523
+outDir <- "~/Desktop/OneDrive - University of Texas at El Paso/CZ_Drylands/JER_RedLakePlaya/Data/eddypro"
+
+# unzip the files into a single folder
+fluxzips <- list.files(path="~/Desktop/OneDrive - University of Texas at El Paso/CZ_Drylands/JER_RedLakePlaya/Data/EddyCovariance_SmartFlux/results/2021/09", full.names=TRUE) 
+plyr::ldply(.data = fluxzips, .fun = unzip, exdir = outDir)
+
+# list the unzipped files with full eddypro output
+flux.files <- list.files(path=paste(outDir,"/output",sep=""), pattern="eddypro_exp_full_output_",full.names=TRUE)
+
+# read the flux files as csv and combine into single dataframe
+flux.units <- fread(flux.files[1], sep=",", dec=".", header=TRUE, skip=1)[1,]
+
+flux.data <- do.call("rbind", lapply(flux.files, header = FALSE, read.table, sep=",", dec=".",
+                                     skip = 3,fill=TRUE, na.strings="NaN",col.names=colnames(flux.units)))
+
+# detach plyr to avoid conflict with dplyr
+detach("package:plyr", unload=TRUE)
+
+
+# try from summaries folder
+flux.files2 <- list.files(path="~/Desktop/OneDrive - University of Texas at El Paso/CZ_Drylands/JER_RedLakePlaya/Data/EddyCovariance_SmartFlux/summaries",full.names=TRUE)
+
+# read the flux files as csv and combine into single dataframe
+flux.units2 <- fread(flux.files2[3], sep="\t", dec=".", header=TRUE, skip=0)[1,]
+
+flux.data2 <- do.call("rbind", lapply(flux.files2[3:22], header = FALSE, fread, sep="\t", dec=".",
+                                     skip = 2, fill=TRUE, na.strings="NaN", col.names=colnames(flux.units2)))
+
 
 # see all column names
 print(biomet.head)
